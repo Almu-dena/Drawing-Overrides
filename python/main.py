f05935e45554fcbafd6bccb5601ef6cede3fa529
Almudena Pereda <almudenapereda@gmail.com>
2023-05-08 11:39:59 -0400

    removed unneeded files and added variable

--- python/main.py
+++ python/main.py
@@ -1,7 +1,9 @@
 import maya.cmds as cmds
 
+
+asset = '*:*:model_GRP' #subject to studio convention rig structure, to be substituted for needed asset
 def drawingOverrides():
-    models = cmds.ls('*:*:model_GRP') #subject to rig structure, to be substitued for needed node
+    models = cmds.ls(asset)
     for m in models:
         cmds.select(m)
         cmds.setAttr((m + '.overrideEnabled'), 0) #if set to 1, turns on drawing overrides
